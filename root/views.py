f595823a8195f8d12bae8be43aeae5dbff4bef4b
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-07 05:52:01 +0100

    - add latest blog

--- root/views.py
+++ root/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from blog.models import Post
 
 
 def home(request):
     """
     Home view function
     """
-
-    return render(request, 'home.html')
+    latest = Post.objects.order_by('created_at')[:2]
+    return render(request, 'home.html', {'latest' : latest})
