53a99069ce58d1e3546c7758c3a10dae9b99789e
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-07 05:49:23 +0100

    - add post details route

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from blog.views import posts
+from blog.views import posts, categories, details
 
 urlpatterns = [
 
     path('', posts, name='posts'),
+    path('posts/<str:cate>/', categories, name='category'),
+    path('posts/<int:id>/', details, name='details'),
 
 ]
