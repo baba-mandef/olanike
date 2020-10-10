13956e52d72d811ef8f759408841120b456770bb
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-11 00:14:23 +0100

    - add update event models

--- event/models.py
+++ event/models.py
@@ -6,10 +6,11 @@ class Event(models.Model):
     Event model
     """
     title = models.CharField(max_length=255)
-    description = models.CharField()
+    description = models.TextField()
     banner = models.ImageField(upload_to='static/img/event')
     start_at = models.DateTimeField()
     finish_at = models.DateTimeField()
+    adress = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title
