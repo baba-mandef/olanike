a1b97b21657b36c1f63876cec8cc80b0c1c6383a
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-14 21:56:34 +0100

    - add views counter field

--- ola/blog/models.py
+++ ola/blog/models.py
@@ -9,11 +9,12 @@ class Post(models.Model):
     post_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     body = models.TextField()
-    banner = models.ImageField(upload_to='static/img/blog')
+    banner = models.ImageField(upload_to='blog_pics')
     category = models.ForeignKey('Category', on_delete=models.CASCADE)  # post category
     created_at = models.DateTimeField(auto_now_add=True,)
     updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    view = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
@@ -24,15 +25,18 @@ class Comment(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
     body = models.TextField()
     author_name = models.CharField(max_length=20)
-    author_pic = models.ImageField(upload_to='static/img/blog/comment', default='static/img/blog/comment/d.png')
+    author_pic = models.ImageField(upload_to='comments_pics')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author_name
 
 
 class Author(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     bio = models.TextField()
-    picture = models.ImageField(upload_to='static/img/blog/author')
+    picture = models.ImageField(upload_to='author_pics')
 
     def __str__(self):
         return self.name
