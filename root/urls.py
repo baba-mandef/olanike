227d0b48009f6ddc33bc87b621414c15245589ae
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-14 21:54:46 +0100

    - add media urls

--- root/urls.py
+++ root/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ola.home.urls')),
     path('blog/', include('ola.blog.urls')),  # add blog path
     path('events/', include('ola.event.urls')),  # add event path
+
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
