b5dd2fba88f840f27fcfab29761ea385e8becba0
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-14 21:56:09 +0100

    - add author_pic field

--- ola/blog/forms.py
+++ ola/blog/forms.py
@@ -5,9 +5,9 @@ from ola.blog.models import Comment
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('body', 'author_name',)
+        fields = ('body', 'author_name', 'author_pic')
         labels = {
             'body': 'Your comment',
             'author_name': 'Your name',
-
+            'author_pic' : 'Your Picture'
         }
