125571a62d1c739387f8785128a326ccb085461e
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-14 18:25:09 +0100

    - update setting.py

--- root/settings.py
+++ root/settings.py
@@ -133,5 +133,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
