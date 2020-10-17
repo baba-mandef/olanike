23ad7ab9e7bdb8d7b3c8f7f69caa515885f5b4b3
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-17 16:36:02 +0100

    - add cloudinary's configs

--- root/prod_setting.py
+++ root/prod_setting.py
@@ -8,3 +8,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = ['olanike.herokuapp.com']
 DATABASES['default'] = dj_database_url.config()
+
+CLOUDINARY_STORAGE = {
+
+    'CLOUD_NAME': os.environ.get('CLOUDNAME'),
+    'API_KEY': os.environ.get('APIKEY'),
+    'API_SECRET': os.environ.get('APISECRET')
+
+}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
