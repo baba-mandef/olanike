bef9d747cd586d141931cfda9845927ee8c553be
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-10-21 09:54:24 +0100

    - bug fix

--- ola/blog/models.py
+++ ola/blog/models.py
@@ -25,7 +25,7 @@ class Comment(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
     body = models.TextField()
     author_name = models.CharField(max_length=20)
-    author_pic = models.ImageField(default='blog_pics/d.png')
+    author_pic = models.ImageField(default='comments_pics/d.png')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
