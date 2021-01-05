150247e8a96416ac85874f88958b28ec1333c003
henri-dev <pariso03henri@gmail.com>
2021-01-05 15:29:27 +0100

    - counter model registration

--- ola/blog/admin.py
+++ ola/blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from ola.blog.models import Category, Post, Comment, Author
+from ola.blog.models import (Category, Post, Comment, Author, Counter)
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Author)
+admin.site.register(Counter)
