36b0691c10fa5bc3c889d1f93c1727066064585d
henri-dev <pariso03henri@gmail.com>
2021-01-24 15:25:57 +0100

    - update home

--- ola/home/views.py
+++ ola/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from ola.blog.models import Post
+from ola.cause.models import Cause
 from ola.event.models import Event
 
 
@@ -7,6 +8,8 @@ def home(request):
     """
     Home view function
     """
-    latest = Post.objects.order_by('created_at')[:2]
-    last_event = Event.objects.order_by('start_at')[:4]
-    return render(request, 'home.html', {'latest': latest, 'last_event': last_event})
+    last_cause = Cause.objects.filter(is_active=True).order_by('created_at')[:2]
+    latest = Post.objects.filter(published=True).order_by('created_at')[:2]
+    last_event = Event.objects.filter(is_active=True).order_by('start_at')[:4]
+    context = {'latest': latest, 'last_event': last_event, 'last_cause':last_cause}
+    return render(request, 'home.html', context)
