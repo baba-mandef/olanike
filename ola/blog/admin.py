079284565b50e09fb424d8358580c0c5d48faf37
henri-dev <pariso03henri@gmail.com>
2021-01-24 15:23:59 +0100

    - update blog

--- ola/blog/admin.py
+++ ola/blog/admin.py
@@ -4,6 +4,6 @@ from ola.blog.models import (Category, Post, Comment, Author, Counter)
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Post)
-admin.site.register(Comment)
+# admin.site.register(Comment)
 admin.site.register(Author)
-admin.site.register(Counter)
+# admin.site.register(Counter)
--- ola/blog/urls.py
+++ ola/blog/urls.py
@@ -4,7 +4,7 @@ from ola.blog.views import posts, categories, details
 urlpatterns = [
 
     path('', posts, name='posts'),
-    path('posts/<str:cate>/', categories, name='category'),
+    path('category/<str:cate>/', categories, name='category'),
     path('post/<str:slug>/', details, name='details'),
 
 ]
