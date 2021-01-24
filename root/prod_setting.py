f5dc8b9af6835fdcef541058d751906b42c90a85
henri-dev <pariso03henri@gmail.com>
2021-01-24 10:02:42 +0100

    - update prod config

--- root/prod_setting.py
+++ root/prod_setting.py
@@ -6,7 +6,7 @@ TEMPLATES_DEBUG = os.environ.get('TEMPLATE_DEBUG')
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = ['olanike.herokuapp.com']
+ALLOWED_HOSTS = ['olanike.herokuapp.com', 'olanike-children.com']
 DATABASES['default'] = dj_database_url.config()
 
 CLOUDINARY_STORAGE = {
